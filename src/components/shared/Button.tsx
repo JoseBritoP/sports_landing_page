953d48fe5af93180135f0503cf2ca9972cd880cd
José Brito <josealbritose@gmail.com>
2024-11-06 12:53:32 -0300

    Button

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+type ButtonType = 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link' | 'nav' 
+interface ButtonTypeProps {
+  type?:ButtonType
+  label:string
+}
+export function ButtonType({type='default',label='Test'}:ButtonTypeProps) {
+  return <Button variant={type}>{label}</Button>
+}
