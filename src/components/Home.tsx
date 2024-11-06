f1730de84276c45fe626337946adbde06131ad29
José Brito <josealbritose@gmail.com>
2024-11-06 13:18:07 -0300

    Home init

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <main className='pt-20'>
+    <main className='pt-20 flex-1'>
       <h1>Hello world</h1>
     </main>
   )
