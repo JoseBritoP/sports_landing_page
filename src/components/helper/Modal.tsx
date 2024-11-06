3971ed60c06df98e8431cf39b99fcb51821b06bd
José Brito <josealbritose@gmail.com>
2024-11-06 18:47:36 -0300

    Hero

--- src/components/helper/Modal.tsx
+++ src/components/helper/Modal.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { FaPlay } from "react-icons/fa";
 
-export default function Modal() {
+export function Modal() {
+
   return (
-    <div className='relative'>
-      <div className='fixed top-0 right-0 bottom-0 left-0 bg-[#000000e1] z-20'/>
-      {/* Main modal */}
-      <div className='fixed top-[50%] left-[50%] right-[50%] bottom-[50%] md:h-[300px lg:w-[800px] lg:-[500px] z-[21] traslate-y-[50%]'>
-        <iframe
-          src='https://www.youtube.com/watch?v=Pir8IUxfVdU&t=2s'
-          className='w-full h-full'
-        >
-        </iframe>
-      </div>
+    <div className="pt-5">
+      <Dialog>
+        <DialogTrigger asChild>
+          <button className="w-[10rem] lg:ml-auto h-[10rem] rounded-full bg-red-600 hover:bg-black transition-all duration-200 cursor-pointer sm:flex flex-col items-center justify-center hidden">
+            <FaPlay className="w-[3rem] h-[3rem] text-white" />
+          </button>
+        </DialogTrigger>
+        <DialogContent className="">
+          <DialogHeader>
+            <DialogTitle ></DialogTitle>
+          </DialogHeader>
+          <div className=" md:h-[300px] lg:w-[800px] lg:-[500px] traslate-y-[50%]">
+            <iframe
+              src="https://www.youtube.com/watch?v=Pir8IUxfVdU&t=2s"
+              className="w-full h-full"
+            ></iframe>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
-
