cf6397acf357805f0ab304879812875f03a25ebc
José Brito <josealbritose@gmail.com>
2024-11-06 16:37:23 -0300

    Styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { ButtonType } from './shared/Button'
 
 export default function Navbar() {
   return (
-    <nav className='bg-gray-50 flex w-full items-center justify-between px-10 py-1 fixed border-b'>
+    <nav className='z-50 bg-gray-50 flex w-full items-center justify-between px-10 py-1 fixed border-b'>
       <Image alt='logo' src='/images/logo.png' height={60} width={60}/>
       <NavCN/>
       <ButtonType type='nav' label='Join us'/>
